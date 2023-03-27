488d21204561b57edcd622e4b211aa44c87c88c5
antonov <artem@antonov.es>
2023-03-28 01:27:31 +0200

    New version.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 const config = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bullet-image': "url('/dot.svg')",
+       },
+      'fontFamily' : {
+        sans: ['var(--asap_condensed-font)', ...fontFamily.serif],
+      }
+    },
   },
   plugins: [],
 };
